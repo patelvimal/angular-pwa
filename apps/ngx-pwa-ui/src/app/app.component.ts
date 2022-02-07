322c3d2b2bc0d0709f57a95d55a0be0e36c1ae89
Vimal Patel <vimal.patel.nvs@gmail.com>
2022-02-07 20:19:28 +0530

    fix: unit test cases

--- apps/ngx-pwa-ui/src/app/app.component.ts
+++ apps/ngx-pwa-ui/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'angular-pwa-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'angular-pwa-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ngx-pwa-ui';
-}
+export class AppComponent {}
--- apps/ngx-pwa-ui/src/app/app.module.ts
+++ apps/ngx-pwa-ui/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, WelcomeComponent],
+    declarations: [AppComponent, WelcomeComponent],
     imports: [
         BrowserModule,
         ServiceWorkerModule.register('ngsw-worker.js', {
